dad05ae1a9d164a87e74d6b79639845351f3b794
Opilane TTHK <opilane@tthk.ee>
2022-01-03 14:13:11 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    class Kordused
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int A = rnd.Next(1,20);
+            int[] massiv = new int[A];
+            int E;
+            for (int i = 0; i < A; i++){
+                E = rnd.Next(0, 10);
+                massiv[i] = E;
+            }
+
+            foreach (var item in massiv)
+            {
+                Console.Write("{0,3}",item);
+            }
+            Console.WriteLine();
+            string[] abc = new string[5] { "A", "B", "C", "D", "E" };
+
+            int ind = rnd.Next(0, 4);
+            Console.WriteLine("Element indeksiga {0} on {1}",ind,abc[ind]);
+
+            int n = abc.Length;
+            int j=0;
+
+            while (n > 0)
+            {
+                Console.WriteLine(abc[n-1]);
+                n--;
+            }
+
+            while (j > abc.Length)
+            {
+                Console.WriteLine(abc[j]);
+                j++;
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Project1
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere," + eesnimi.ToUpper());
+            if (eesnimi.ToLower() == "juku") {
+                Console.WriteLine("LAHME KINNO {0}", eesnimi, "Kui vana sa oled?");
+                int vanus = int.Parse(Console.ReadLine());
+                double hind = 6.50;
+                string pilet;
+                if (vanus < 0 || vanus > 120)
+                {
+                    Console.WriteLine("Viga!");
+                }
+                else
+                {
+                    if (vanus < 6)
+                    {
+                        pilet = "Suur soodustus!";
+                        hind *= 0.5;
+                    }
+                    else
+                    {
+                        pilet = "Taispilet!";
+                    }
+
+                    Console.WriteLine("Sul in {0} pileti hind on {1}", pilet, hind);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mina olen hõivatud");
+            }
+
+            /*
+             Console.ReadLine();
+             int arv1= int.Parse(Console.ReadLine());
+             int arv2 = int.Parse(Console.ReadLine());
+             Console.WriteLine("Arvude {1}ja {0}korrutis võrdub{2}", arv1, arv2, arv1 * arv2);
+             */
+
+        }
+    }
+
+
+
+
+
+    class lesson {
+
+
+    static void Red(int arv, string tried)
+    {
+
+
+
+    }
+
+
+    }
+
+
+
+}
